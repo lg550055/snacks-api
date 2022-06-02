c570671ca44c42038a5c811865ab3885ae2fae64
lg550055 <polospam@outlook.com>
2022-06-02 13:31:39 -0500

    Adds redirectview to empty path

--- snacks_api/urls.py
+++ snacks_api/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
+    # uses redirectview to avoid 404 on empty path, which has no django view
+    path('', RedirectView.as_view(url='api/v1/snacks/')),
     path('admin/', admin.site.urls),
     path('api/v1/snacks/', include('snacks.urls'))
 ]
